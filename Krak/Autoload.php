7607a39b08ed30d56d4159a747d0a75a4889ee62
RJ garcia <rj@bighead.net>
2013-08-24 11:19:54 -0700

    Adding table prefixes
    Signed-off-by: RJ garcia <rj@bighead.net>

--- Krak/Autoload.php
+++ Krak/Autoload.php
@@ -20,12 +20,15 @@ if ($mloader instanceof iLoader == false)
 	throw new Exception(get_class($mloader) . " returned from " . USER_PATH . "Autoload.php is not an instance of Krak\iLoader.");
 }
 
+// make sure this constant was defined
+defined('Krak\USE_NS_AS_PREFIX') || define('Krak\USE_NS_AS_PREFIX', false);
+
 // register the loader
 $mloader->register();
 
 $mpackage = $mloader->get_package();
 
-define('Krak\MODEL_NS_LEN', ($mpackage) ? strlen($mpackage) + 1 : 0);
+define('Krak\MODEL_NS_LEN', ($mpackage && !USE_NS_AS_PREFIX) ? strlen($mpackage) + 1 : 0);
 
 // now let's initialize Krak
 Model::init();
